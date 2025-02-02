58744cacb95ab78a7e34ca8a34406ede6cbccefe
adi001 <iamadi@001.com>
2025-02-02 23:06:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const username = "Aditya Singh";
+const age = 25;
+const chessrating_rapid = 500;
+
+function printHello(name) {
+    console.log(`Hello my name is ${name}`);
+}
+
+printHello(username);
