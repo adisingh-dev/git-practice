e7fc2d04058f32ff36feae7cb732d5537cc41fb1
adi001 <iamadi@001.com>
2025-02-04 21:51:56 +0530

    added return value to printhello

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const age = 25;
 const chessrating_rapid = 500;
 
 function printHello(name) {
-    console.log(`Hello my name is ${name}`);
+    return `Hello my name is ${name}`;
 }
 
 printHello(username);
