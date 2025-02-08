ae35dd8995fe322ed802f9803732944c1fb397b4
adi001 <iamadi@001.com>
2025-02-08 15:50:02 +0530

    add new piece knight

--- utls.js
+++ utls.js
@@ -1,8 +1,9 @@
-const pieces = ['king', 'queen'];
+const pieces = ['king', 'queen', 'knight'];
 
 const getStrength = function(piece) {
     switch (piece) {
         case "king": return 10;
         case "queen": return 9;
+        case "knight": return 3;
     }
 }
