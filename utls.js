dcdaa026f9f252fb337775ec9706d2ecf2db2d76
adi001 <iamadi@001.com>
2025-02-08 15:41:33 +0530

    added moves for available pieces

--- utls.js
+++ utls.js
@@ -6,3 +6,12 @@ const getStrength = function(piece) {
         case "queen": return 9;
     }
 }
+
+const movePiece = function(piece) {
+    let moves = [[0, 0]];
+    switch(piece) {
+        case "king": moves = [[+1, 0], [+1, -1], [0, -1], [-1, -1], [-1, 0], [-1, +1], [0, +1], [+1, +1]];
+        case "knight": moves = [[+1, +2], [-1, +2], [-2, +1], [-2, -1], [+1, -2], [-1, -2], [+2, +1], [+2, -1]];
+    }
+    return moves;
+}
